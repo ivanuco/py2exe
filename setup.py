fdb87bdd62e310c33db84f2f08ffacf7b03485d5
Iván del Viejo <ivandelviejo@gmail.com>
2017-06-09 20:57:25 +0200

    Inicial

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import py2exe
+import sys
+import re
+
+setup(name="Hola mundo",
+      version="0.1",
+      description="Ejemplo del funcionamiento de distutils",
+      author="Ivan del Viejo",
+      author_email="ivandelviejo@gmail.com",
+      url="https://github.com/ivanuco",
+      license="GPL",
+      scripts=['./src/hello/hello.py'],
+      console=['./src/hello/hello.py'],
+      options={"py2exe": {"bundle_files": 1}},
+      zipfile=None
+    )
---
+++ src/hello/hello.py
@@ -0,0 +1,6 @@
+'''
+Created on 9 jun. 2017
+
+@author: ivanuco
+'''
+print("Hello World!")
